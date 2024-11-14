f59d3966b5adb5b66d4511b62cf247f8644fbe6c
Dartpopper1 <espinozaj@roverkids.org>
2024-11-14 14:02:49 -0500

    Finished 1.2

--- Protype 1/Assets/Scenes/Scripts/PlayerControl.cs
+++ Protype 1/Assets/Scenes/Scripts/PlayerControl.cs
@@ -11,6 +11,7 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // We'll Move the Vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
